e7b4ccbb77f67ce7f1b3618a5ae7bec37f52c32a
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2019-09-27 15:15:47 -0500

    Add setup.py

---
+++ intron/__about__.py
@@ -0,0 +1,23 @@
+"""Central place for package metadata."""
+
+# NOTE: We use __title__ instead of simply __name__ since the latter would
+# interfere with a global variable __name__ denoting object's name.
+__title__ = "intron"
+__summary__ = "Intron retention tool"
+__url__ = "https://github.com/jareyw/intron/"
+__version__ = "0.0.1"
+__author__ = "Therapeutic innovation center, Baylor college of medicine"
+__email__ = "info@genialis.com"
+__license__ = "Apache License 2.0"
+__copyright__ = "2019, " + __author__
+
+__all__ = (
+    "__title__",
+    "__summary__",
+    "__url__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+)
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os.path
+import setuptools
+
+# Get the long description from README.
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+# Get package metadata from '__about__.py' file.
+about = {}
+base_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(base_dir, "intron", "__about__.py"), "r") as fh:
+    exec(fh.read(), about)
+
+setuptools.setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author=about["__author__"],
+    author_email=about["__email__"],
+    url=about["__url__"],
+    license=about["__license__"],
+    # Exclude tests from built/installed package.
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*"]),
+    python_requires=">=3.6, <3.8",
+    install_requires=["numpy", "pybedtools", "pysam"],
+    extras_require={
+        "docs": ["sphinx", "sphinx_rtd_theme"],
+        "package": ["twine", "wheel"],
+        "test": ["black", "check-manifest", "docutils", "flake8", "isort", "pydocstyle", "pytest-cov"],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alfa",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="bioinformatics intron RNA mRNA",
+)
