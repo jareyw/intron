a9850695846607f877d2795144e8cb9c2277a8c6
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2020-01-20 12:21:43 +0100

    Port intronNominator_v1.1.py to Python3

--- legacy/intronNominator_v1.1.py
+++ legacy/intronNominator_v1.1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import re
@@ -19,16 +19,16 @@ try:
 	opts, args = getopt.getopt(sys.argv[1:],'h:g:m:l:d:r:f:o:',['gtffolderpath=','metric=','readlength=','datatype=','ranktype=','filter','output='])
 except getopt.GetoptError as err:
 	print (err)
-	print "'gtffolderpath=','metric=','readlength','datatype=','ranktype=','filter','output='"
+	print("'gtffolderpath=','metric=','readlength','datatype=','ranktype=','filter','output='")
 	sys.exit(2)
 
-print "'gtffolderpath=','metric=','readlength','datatype=','ranktype=','filter','output='"
+print("'gtffolderpath=','metric=','readlength','datatype=','ranktype=','filter','output='")
 cons_dict = {} #initialize to empty dictionary, which is default
 
 for opt, arg in opts:
 	if opt == '-h':
 		sys.exit()
-		print "Documentation not complete. See administrator."
+		print("Documentation not complete. See administrator.")
 	elif opt in ('-g','--gtffolderpath'):
 		folderpath = arg
 	elif opt in ('-m','--metric'):
@@ -44,7 +44,7 @@ for opt, arg in opts:
 	elif opt in ('-o','--output'):
 		output = arg
 
-print output
+print(output)
 fileout = open('%s.txt' % (output),'w')
 
 '''
@@ -65,7 +65,7 @@ Main Code
 d={}
 i = 0
 for file in glob.glob("%s/*.gtf" % (folderpath)):
-	print file
+	print(file)
 	count=0
 	subcount=0
 
@@ -80,7 +80,7 @@ for file in glob.glob("%s/*.gtf" % (folderpath)):
 		try:
 			mr = intron_count*1000/float(length*rpkm)
 			if mr:
-				print mr
+				print(mr)
 				break
 		except:
 			continue
@@ -93,7 +93,7 @@ for file in glob.glob("%s/*.gtf" % (folderpath)):
 		sample_id = file.split('/')[-1][:-4] #modify depending on what you want your sample names to be
 
 		intron_id = line[-4].split(' ')[-1][:-1]+'.'+str(start) #custom intron id
-		
+
 		length = end-start
 		added_length = length+2*(readlength-1)
 
@@ -123,7 +123,7 @@ for file in glob.glob("%s/*.gtf" % (folderpath)):
 			spkm = span_count*1000.0/float(2*(readlength-1)*mr)
 		else:
 			spkm = 0.0
-		
+
 		try:
 			ratio = float(rpkm/spkm)
 		except:
@@ -154,17 +154,17 @@ for intron in d:
 	#get rid of the following lines before appending to the intron_list
 	#filter for minimum spkm > 20 (not sure how much this will impact the output) - will have to test run
 	if any(i<spkm_min for i in d[intron]['span_count']):
-		print d[intron]['sample_id']
-		print d[intron]['span_count']
+		print(d[intron]['sample_id'])
+		print(d[intron]['span_count'])
 		continue
-	
+
 	#filter for reasonable ratio of true intron reads/spanning reads
 	if d[intron]['ratio_mean'] > 1: #weakness of applying this filter is that many of the "IR" features it cuts are cassette exons
 		continue
 
 	intron_list.append(d[intron])
 
-print len(intron_list)
+print(len(intron_list))
 
 #option for metric used (so far, sum and median)
 #sorts by whichever metric you decide on
@@ -179,7 +179,7 @@ fileout.writelines('intron_id'+'\t'+'info'+'\t')
 sample_id_string = '\t'.join(sample_id_list)
 fileout.write(sample_id_string)
 fileout.write('\n')
-print sample_id_list
+print(sample_id_list)
 
 for item in high_values:
 	fileout.write(item['intron_id']+'\t')
