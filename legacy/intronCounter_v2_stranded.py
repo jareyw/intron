6821406a109173a59203996d572e3c1884b0bfa5
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2020-01-27 13:47:20 +0100

    Prettify intronCounter_v2_stranded.py script

--- legacy/intronCounter_v2_stranded.py
+++ legacy/intronCounter_v2_stranded.py
@@ -1,122 +1,68 @@
 #!/usr/bin/python3
+"""Intron counter."""
+import argparse
 
+import pybedtools
 import pysam
-import sys
-import re
-import getopt
-import multiprocessing
 
-#current iteration assumes .bam inputs, reads in gtf file, takes in output location
 
-#gtffile should be limited to only introns
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Count number of unprocessed reads in each intron.")
+    parser.add_argument("-b", "--bam", help="Input BAM file.", required=True)
+    parser.add_argument("-g", "--gtf", help="Input GTF file with introns.", required=True)
+    parser.add_argument("-o", "--output", help="Output file.", required=True)
+    return parser.parse_args()
 
-try:
-	opts, args = getopt.getopt(sys.argv[1:],'h:b:g:o:',['bamfolderpaths=','gtffile=','output='])
-except getopt.GetoptError as err:
-	print(err)
-	sys.exit(2)
-
-for opt, arg in opts:
-	if opt == '-h':
-		sys.exit()
-	elif opt in ('-b','--bamfolderpaths'):
-		id_list = []
-		for line in open(arg,'r').readlines():
-			line = line.split('\t')
-			id_list.append(line)
-	elif opt in ('-g','--gtffile'):
-		intron_list=[]
-		for line in open(arg,'r').readlines():
-			intron_list.append(line[0:-1])
-		gtf = arg
-	elif opt in ('-o','--output'):
-		output = arg
 
 def is_read_on_correct_strand(strand, read):
-	if strand == "+" and ((read.is_read1 and read.is_reverse) or (read.is_read2 and not read.is_reverse)):
-		return True
-	elif strand == "-" and ((read.is_read1 and not read.is_reverse) or (read.is_read2 and read.is_reverse)):
-		return True
+    """Check if read is on expected strand."""
+    if strand == "+" and ((read.is_read1 and read.is_reverse) or (read.is_read2 and not read.is_reverse)):
+        return True
+    elif strand == "-" and ((read.is_read1 and not read.is_reverse) or (read.is_read2 and read.is_reverse)):
+        return True
 
-	return False
+    return False
 
-def intronCounter(file,short_id,intron_list,output,count):
-	fileout = open(output + '/%s.gtf' % (short_id.rstrip()),'w')
-	#write initial row of intron ids
 
-	count+=1
-	print(count)
+def intronCounter(file, gtf_file, output_file):
+    """Count number of reads in intron."""
+    with pysam.AlignmentFile(file, "rb") as bamfile, open(output_file, "wt") as fileout:
+        for intron in pybedtools.BedTool(gtf_file):
+            fileout.write(str(intron).rstrip("\n") + "\t")
 
-	print(file)
-	print(pysam.idxstats(file))
+            if len(intron) <= 10:
+                fileout.write("0" + "\n")
+                continue
 
-	bamfile = pysam.AlignmentFile(file,'rb')
+            intron_count = 0
+            span_count = 0
+            for read in bamfile.fetch(intron.chrom, intron.start + 5, intron.end - 5):
+                if read.is_duplicate:
+                    continue
+                if read.is_qcfail:
+                    continue
+                if read.is_secondary:
+                    continue
+                if not read.is_proper_pair:
+                    continue
 
-	#calculate mappable reads for tcga (star) bams
-	#mapped_reads = reduce(lambda x, y: x + y, [int(l.split('\t')[2]) for l in pysam.idxstats(file)])
+                # This check is for ISR reads:
+                # Filtering by correct strand is done for ISR type (Illumina truseq):
+                # https://salmon.readthedocs.io/en/latest/library_type.html
+                # TODO: Generalize this for different types of kits.
+                if is_read_on_correct_strand(intron.strand, read):
 
-	#calculate mappable reads for hisat generated bams
-	#mapped_reads = reduce(lambda x, y: x + y, [int(l.split('\t')[2]) for l in pysam.idxstats(file).rstrip('\n').split('\n')])
-	#mapped_frags = float(mapped_reads/2) #this is over-conservative, since some frags will only have 1 mapped read (not 2)
+                    span_count += 1
+                    if "N" not in read.cigarstring:
+                        intron_count += 1
 
-	mapped_reads = bamfile.mapped
-	print(mapped_reads)
+            # Calculate rpkm
+            rpkm = (float(intron_count) * 10 ** 9) / (bamfile.mapped * len(intron))
 
-	subcount=0
-	for newline in intron_list:
-		line = newline.split('\t')
+            fileout.write("\t".join(map(str, [intron_count, span_count, rpkm])) + "\n")
 
-		subcount+=1
-		if subcount %10000 == 0:
-			print(subcount)
 
-		chrom = line[0]
-		start = int(line[3])
-		end = int(line[4])
-		strand = line[6]
-
-		fileout.write(newline + '\t')
-
-		if end - start <= 10:
-			fileout.write('0' + '\n')
-			continue
-
-		intron_count=0
-		span_count=0
-		for read in bamfile.fetch(chrom,start+5,end-5):
-			if read.is_duplicate:
-				continue
-			if read.is_qcfail:
-				continue
-			if read.is_secondary:
-				continue
-			if not read.is_proper_pair:
-				continue
-
-			# This check is for ISR reads:
-			# Filtering by correct strand is done for ISR type (Illumina truseq):
-    		# https://salmon.readthedocs.io/en/latest/library_type.html
-    		# TODO: Generalize this for different types of kits.
-			if is_read_on_correct_strand(strand, read):
-
-				span_count+=1
-				if 'N' not in read.cigarstring:
-					intron_count+=1
-
-		#calculate rpkm per genomic locus for every intron
-		intron_length = end - start
-		reads_per_mil = float(mapped_reads/1000000)
-		rpm = float(intron_count)/reads_per_mil
-		rpk = float(intron_length)/1000
-		rpkm = float(rpm)/rpk
-
-		fileout.write(str(intron_count)+'\t'+str(span_count)+'\t'+str(rpkm)+'\n')
-	fileout.close()
-
-if __name__ == '__main__':
-	jobs=[]
-	for file, short_id in id_list:
-		count=0
-		p = multiprocessing.Process(target=intronCounter,args=(file,short_id,intron_list,output,count))
-		jobs.append(p)
-		p.start()
+if __name__ == "__main__":
+    args = parse_arguments()
+    intronCounter(args.bam, args.gtf, args.output)
